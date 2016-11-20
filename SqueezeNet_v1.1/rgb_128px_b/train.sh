8756ba3eeed0080ad44d878c08f2702666fd836b
Yossi Biton <yossi.biton@adasky.com>
2016-11-20 14:21:27 +0200

    updates

--- SqueezeNet_v1.1/rgb_128px_b/train.sh
+++ SqueezeNet_v1.1/rgb_128px_b/train.sh
@@ -1,2 +1,2 @@
-OUTPUT=../../output/SqueezeNet_v1.1/grayscale_128px/log
+OUTPUT=../../output/SqueezeNet_v1.1/rgb_128px_b/log
 ~/Dev/faster_rcnn/external/caffe/build/tools/caffe train -gpu 0 -solver solver.prototxt 2>&1 | tee $OUTPUT
